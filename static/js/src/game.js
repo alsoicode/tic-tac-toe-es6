5d952e31144abc9ff31d2677647d2aaaf4bdefd5
Brandon Taylor <alsoicode@gmail.com>
2016-01-24 09:44:21 -0500

    Beginning of Game
    Added start of game.js in ES6

---
+++ static/js/src/game.js
@@ -0,0 +1,16 @@
+export class Game {
+
+    constructor() {
+        this.winningCombos = [
+            // horizontal
+            [1, 2, 3], [4, 5, 6], [7, 8, 9],
+
+            // vertical
+            [1, 4, 7], [2, 5, 8], [3, 6, 9],
+
+            // diagonal
+            [1, 5, 9], [3, 5, 7]
+        ];
+    }
+
+}
